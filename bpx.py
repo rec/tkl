f30bc8d5182bd3ea89a4b04c2ef3048b3c363cc4
Tom Ritchford <tom@swirly.com>
2021-04-17 15:39:07 +0200

    First working version

--- bpx.py
+++ bpx.py
@@ -1,4 +1,6 @@
 from bibliopixel.drivers.driver_base import DriverBase
+from bibliopixel.drivers.simpixel import SimPixel
+from xled.realtime import RealtimeChannel
 import xled
 
 
@@ -6,50 +8,61 @@ class BPX(DriverBase):
     """
     A BiblioPixel driver for the xled driver for Twinkly™ lights.
     """
-
-    def __init__(
-        self, use_white=False, ip_address=None, hw_address=None, **kwds
-    ):
+    def __init__(self, *args, address=None, white_ratio=1, **kwds):
         """
         Args:
-            num:  number of LEDs on the Twinkly.
+            address
+                If address is None, discover any LED string.
+                Otherwise, address must be a pair of strings representing
+                IP address and HW address, or a string containing those
+                addresses separated by a /
 
-            use_white:
-                False means three colors.
-                True means four colors, with white at the average of RGB.
-                A number means four colors, but scale the white level.
-                Scaling beyond 1 is possible but can result in clipping.
+            white_ratio
+                For RGBW LEDs, the white component is the average of the
+                RGB components, scaled by `white_ratio` (default 1)
 
             **kwds:  keywords passed to DriverBase.
         """
-        dd = xled.discover.discover()
-        self.control = xled.ControlInterface(
-            ip_address or dd.ip_address, hw_address or dd.hw_address,
-        )
-        self.control.set_mode("rt")
+        super().__init__(*args, **kwds)
+        self.white_ratio = white_ratio
 
-        numLEDs = self.control.get_device_info()["number_of_led"]
+        if not address:
+            dd = xled.discover.discover()
+            address = dd.ip_address, dd.hw_address,
+        elif isinstance(address, str):
+            address = address.split('/')
+        if len(address) != 2:
+            raise ValueError(f'Bad address {address}')
 
-        super().__init__(numLEDs, **kwds)
+        self.control = xled.ControlInterface(*address)
+        self.info = self.control.get_device_info()
 
-        assert self.use_white >= 0
-        self.use_white = use_white
-        self.rgbw = use_white is not False and bytearray(4 * numLEDs)
+        actual_leds = self.info['number_of_led']
+        if actual_leds != self.numLEDs:
+            raise ValueError(f'Set shape: {actual_leds} in the BP project')
+
+        self.colors_per_led = len(self.info['led_profile'])
+        if self.colors_per_led != 3:
+            assert self.colors_per_led in 1, 4
+            self.buffer = bytearray(self.colors_per_led * num)
+        else:
+            self.buffer = self._buf
+        self.rtc = RealtimeChannel(self.control, 250, 3)
+        self.rtc.start_realtime()
+
+    def _compute_packet(self):
+        self._render()
+        if self.colors_per_led == 3:
+            return
+
+        # TODO: use numpy
+        for i in range(self.numLEDs):
+            color = self._buf[3 * i : 3 * i + 3]
+            white = min(255, int(self.use_white * sum(color) / 3))
+            if self.colors_per_led == 1:
+                self.buffer[i] = white
+            else:
+                self.buffer[4 * i : 4 * i + 4] = (*color, white)
 
     def _send_packet(self):
-        # Called on each BiblioPixel update
-        self.control.set_rt_message(self._buffer())
-        # xled.control.ControlInterface.set_rt_message() doesn't exist yet!
-
-    def _buffer(self):
-        # Returns a bytearray looking like this:
-        #    RGBRGBRGB... if use_white is False
-        #    RGBWRGBWRGBW... if use_white is True, or any number
-
-        if self.rgbw:
-            for i in range(self.numLEDs):
-                color = self._buf[3 * i : 3 * i + 3]
-                white = min(255, int(self.use_white * sum(color) / 3))
-                self.rgbw[4 * i : 4 * i + 4] = (*color, white)
-
-        return self.rgbw or self._buf
+        self.rtc.send_frame(self.buffer)
